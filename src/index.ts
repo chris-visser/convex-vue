cac17d50e60a0f4b878d74922d00b1b902f6836e
Chris Visser <2496739+chris-visser@users.noreply.github.com>
2025-05-20 11:49:01 +0200

    Test

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-export { useConvexClient } from './composables/useConvexClient'
+export { useConvexClient } from "./composables/useConvexClient";
 
-export { useConvexHttpQuery } from './composables/useConvexHttpQuery'
-export { useConvexMutation } from './composables/useConvexMutation'
-export { useConvexQuery } from './composables/useConvexQuery'
-export { convexVue } from './plugin'
+export { useConvexHttpQuery } from "./composables/useConvexHttpQuery";
+export { useConvexMutation } from "./composables/useConvexMutation";
+export { useConvexQuery } from "./composables/useConvexQuery";
+export { convexVue } from "./plugin";
+console.log("???");
